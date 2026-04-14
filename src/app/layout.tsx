9810f2971914f3d3376b46d5f841d384dd080f95
Jasmin Mistry <mistry.jasmin@gmail.com>
2026-04-14 10:01:57 +0530

    header GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import { Analytics } from "@vercel/analytics/next";
 import { Suspense } from "react";
+import Script from "next/script";
 import { Toaster } from "sonner";
 import Header from "@/components/header";
 import { Footer } from "@/components/footer";
@@ -33,6 +34,31 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en" className="root">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-QTXQ1H7HG2"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-QTXQ1H7HG2');
+          `}
+        </Script>
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "t3kivscorp");
+          `}
+        </Script>
+      </head>
       <body
         className={`${GeistSans.variable} ${GeistMono.variable} font-base`}
         
