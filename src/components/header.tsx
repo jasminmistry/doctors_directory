823c2388aadaef98694f6fa620eaae715cdd2222
Jasmin Mistry <mistry.jasmin@gmail.com>
2026-05-27 10:14:07 +0530

    critical issue fixes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -28,6 +28,7 @@ export default function Header() {
     normalizedPath !== "/clinics" &&
     normalizedPath !== "/practitioners" &&
     normalizedPath !== "/products" &&
+    normalizedPath !== "/products/brands" &&
     normalizedPath !== "/treatments" &&
     !normalizedPath.startsWith("/accredited");
 
